80b1046dcad2651a886eb02668c8dba8afe34d32
Whilreal0 <whil.real@gmail.com>
2023-01-19 12:19:54 +0800

    crud

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-main>
+    <Todo />
+  </v-main>
+</template>
+<script>
+import Todo from "@/components/Todo";
+
+export default {
+  name: "home",
+
+  data: () => ({
+    //
+  }),
+  components: {
+    Todo,
+  },
+};
+</script>
+<style >
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+}
+</style>
